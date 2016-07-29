236a564620d570b3c7fd2554c7aa065c69437fc6
AbhishekPalsule <abhishek.palsule@gmail.com>
2016-07-29 18:50:31 +0530

    Add files via upload

--- Player.java
+++ Player.java
@@ -7,10 +7,11 @@ import java.util.Objects;
 public class Player {
     final int SIZE = 13;
     private ArrayList<Card> hand;
+    int currentBid;
     int score;
     int remaining;
     public Player(ArrayList<Card> hand){
-        this.hand = hand;
+         this.hand = hand;
         score = 0;
         remaining = 104;
     }
@@ -18,9 +19,32 @@ public class Player {
     public int bid (int value){
         if(hand.contains(value)){
            hand.remove((Object)value);
+            currentBid = value;
             return value;
         }
     return -1;
     }
-    
+
+
+    public int getScore() {
+        return score;
+    }
+
+
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+
+    public String hand(){
+        return hand.toString() ;
+    }
+
+    public void updateStatus(boolean won,int bidFor){
+        if(won){
+            score += bidFor;
+        }
+        remaining -= currentBid;
+    }
 }
